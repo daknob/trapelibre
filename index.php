f679df989cf9f50dcbf1b9e598e6ffdec3288522
Spyros Panagiotopoulos <martkonsta@gmail.com>
2016-08-10 22:35:01 +0300

    add basic navbar

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+	<link href="static/css/navbar.css" rel="stylesheet">
+	<link href="static/css/bootstrap.min.css" rel="stylesheet">
+	</head>
+	<body>
+		<?php
+			require "navbar.html";
+		?>
+	</body>
+
+</html>
