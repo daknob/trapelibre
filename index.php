98ded37f5dbf0431ecb45d11494aab333cbdee41
DaKnOb <daknob.mac@gmail.com>
2016-08-11 17:47:50 +0300

    This commit:
    - Adds a login form in index.php
    - Fixes a require issue in index.php
    - Adds footer.php
    - Adds footer.php in index.php

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
 		See the License for the specific language governing permissions and
 		limitations under the License.
 	*/
+	
+	require_once("config.php");
 ?>
 
 <html>
@@ -24,7 +26,42 @@
 	</head>
 	<body>
 		<?php
-			require "navbar.php";
+			require_once "navbar.php";
+		?>
+		<div class="container">
+			<div class="row">
+				<div class="panel panel-default col-xs-offset-1 col-xs-10">
+					<div class="panel-heading">
+						<h3 class="panel-title">Log-in</h3>
+					</div>
+					<div class="panel-body">
+						Welcome to the <?php echo $ProjectName; ?> instance for <?php echo $RestaurantName; ?>.
+						In order to continue to the system, please log-in.
+						
+						<form action="login.php" method="post">
+							<div class="row">
+								<div class="form-group col-xs-offset-1 col-xs-10 col-md-4">
+									<label for="username">Username:</label>
+									<input type="text" class="form-control" id="username" name="username"></input>
+								</div>
+								<div class="form-group col-xs-offset-1 col-xs-10 col-md-offset-2 col-md-4">
+									<label for="password">Password:</label>
+									<input type="password" class="form-control" id="password" name="password"></input>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-xs-offset-1 col-xs-10">
+									<button type="submit" class="btn btn-primary col-xs-offset-1 col-xs-10">Log-in</button>
+								</div>
+							</div>
+						</form>
+					
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php
+		require_once "footer.php";
 		?>
 	</body>
 
